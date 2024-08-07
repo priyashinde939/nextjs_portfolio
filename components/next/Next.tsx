c6378ffb65ab04e3dc1920dd2e5810d97f374b59
priya shinde <priya.shinde9390@gmail.com>
2024-08-07 22:55:57 +0530

    h

--- components/next/Next.tsx
+++ components/next/Next.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Sec = () => {
     return (
-        <div className=" bg-black-100 h-[100vh]">
+        <div className=" bg-gradient-to-b from-black-100 to-black-200 h-[100vh]">
             
         </div>
     )
