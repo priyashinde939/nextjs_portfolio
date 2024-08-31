d78d78925b82e766ea6761769bbbfaccd32c88b6
priya shinde <priya.shinde9390@gmail.com>
2024-09-01 03:13:19 +0530

    d

--- components/Planet/Planet.tsx
+++ components/Planet/Planet.tsx
@@ -7,8 +7,15 @@ const Scene = dynamic(()=> import('@/components/Planet/Scene'), {ssr:false});
 const Planet = () => {
     return (
         <section className="relative h-[130vh] w-full bg-black-100 ">
+            <h1 className="absolute z-10 text-8xl text-white-200 font-black items-center justify-center p-10">
+                SED CONDIMENTUM MUS AUGUE LUCTUS 
+            </h1>
+        <div 
+            className=" absolute pt-60 left-[60vw] m-0 h-auto w-[600px] text-2xl text-start p-10 text-gray-500">
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium dolor laudantium, totam rem aperiam, ipsa quae abeaque ipsa quae ab illo ipsa quae abeaqueinventore veritatis 
+            
+        </div>
             <Scene />
-            <Home />
         </section>
     )
 }
