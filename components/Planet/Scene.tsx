4dafc8e1bbbe21236284eb54ad97282b9aa17b48
priya shinde <priya.shinde9390@gmail.com>
2024-09-01 02:56:32 +0530

    s

--- components/Planet/Scene.tsx
+++ components/Planet/Scene.tsx
@@ -3,6 +3,7 @@ import { Canvas } from '@react-three/fiber';
 import { Suspense } from 'react';
 import Model from './Model';
 import { useProgress, Html } from '@react-three/drei';
+import { OrbitControls} from '@react-three/drei';
 
 function Loader() {
   const { progress } = useProgress();
@@ -13,10 +14,11 @@ export default function Scene() {
   return (
     <Canvas
       style={{ height: '100vh', width: '100%' }}
-      camera={{ position: [-10, -5, 12], fov: 10, far: 60}}
+      camera={{ position: [0, 0, -10], fov: 30, far: 70}}
       gl={{ antialias: true }}
       dpr={[1.5, 2]}
     >
+      <OrbitControls enableZoom={false} enablePan={false} autoRotate={true} />
       <directionalLight position={[-5, -5, 10]} intensity={2} />
       <ambientLight intensity={0.9} color='white' />
       <Suspense fallback={<Loader />}>
